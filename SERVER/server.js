093520bab1f9d6ec69b275dfba1f0292c4cec1d4
varshapanda <varsha.vijayashwini@kalvium.community>
2025-03-26 10:49:33 +0530

    Implemented authentication (username / password) in application

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// Imported databse connection function
+// Imported database connection function
 const connectDatabase = require('./config/database.js');
 const authRoutes = require('./routes/authRoutes.js');
 const artworkRoutes = require('./routes/artworkRoutes.js');
