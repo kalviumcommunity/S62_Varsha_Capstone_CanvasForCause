9c97cfe9a8d35544b1d6fcd1c7e45fcd6657e87c
varshapanda <varsha.vijayashwini@kalvium.community>
2025-03-26 10:35:50 +0530

    Implemented authentication (username / password) in application

--- SERVER/config/database.js
+++ SERVER/config/database.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
-// Import Mongoose to handle MongoDB connections
+require('dotenv').config();// Import Mongoose to handle MongoDB connections
 const mongoose = require('mongoose');
 
 // Database Connection: Establish a connection to MongoDB database
-const connectDatabase = async () => {
-  try {
-    // Connect to MongoDB using the database URI from environment variables
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB Connected Successfully');
-  } catch (error) {
-    console.error('MongoDB Connection Error:', error);
-    process.exit(1);
-  }
+const connectDatabase = () => {
+  // Connect to MongoDB using the database URI from environment variables
+  mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log('MongoDB Connected Successfully');
+    })
+    .catch((error) => {
+      console.error('MongoDB Connection Error:', error);
+    });
 };
 
-// Export the database connection function to be used in the main application
 module.exports = connectDatabase;
