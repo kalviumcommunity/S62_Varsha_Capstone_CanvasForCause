ae0a42c93e2218753d00efcb26196230fb06f856
varshapanda <varsha.vijayashwini@kalvium.community>
2025-08-04 02:29:48 +0530

    Configure CORS to allow frontend origins

--- SERVER/server.js
+++ SERVER/server.js
@@ -20,7 +20,7 @@ connectDatabase();
 
 app.use(generalLimiter);
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:['https://s62-varsha-capstone-canvas-for-cause.vercel.app/','http://localhost:5173'],
   credentials:true
 }));
 app.use(express.json({limit:'10mb'}));
